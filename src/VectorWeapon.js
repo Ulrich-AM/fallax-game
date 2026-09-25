56a2f9ab0412948da70e2c736dd5f8334c3487d8
Ulrich AM <ulrichspaceprogram@gmail.com>
2026-09-25 11:15:22 +0800

    Version Vector for death menu build

--- src/VectorWeapon.js
+++ src/VectorWeapon.js
@@ -1,4 +1,4 @@
-import { rectangle, group, rasterize } from './pixelShapes.js?v=12';
+import { rectangle, group, rasterize } from './pixelShapes.js?v=13';
 
 function degToRad(degrees) {
   return degrees * Math.PI / 180;
