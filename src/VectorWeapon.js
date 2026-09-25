7b79e10181e64cf5c2645471b4dcfcf022433c6c
Ulrich AM <ulrichspaceprogram@gmail.com>
2026-09-25 11:06:50 +0800

    Version Vector renderer for expanded attacks

--- src/VectorWeapon.js
+++ src/VectorWeapon.js
@@ -1,4 +1,4 @@
-import { rectangle, group, rasterize } from './pixelShapes.js?v=11';
+import { rectangle, group, rasterize } from './pixelShapes.js?v=12';
 
 function degToRad(degrees) {
   return degrees * Math.PI / 180;
