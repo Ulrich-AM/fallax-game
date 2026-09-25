30c2c06d169c624db098313cf2381d2c6e2455af
Ulrich AM <ulrichspaceprogram@gmail.com>
2026-09-25 23:34:50 +0800

    Align dependencies for Matrix v26

--- src/VectorWeapon.js
+++ src/VectorWeapon.js
@@ -1,4 +1,4 @@
-import { rectangle, group, rasterize } from './pixelShapes.js?v=25';
+import { rectangle, group, rasterize } from './pixelShapes.js?v=26';
 
 function degToRad(degrees) {
   return degrees * Math.PI / 180;
