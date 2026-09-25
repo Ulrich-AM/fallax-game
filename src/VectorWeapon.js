679024c4ff97eba757478159ac0f45272c94e413
Ulrich AM <ulrichspaceprogram@gmail.com>
2026-09-25 17:53:40 +0800

    Align weapon renderer cache for Horizon build

--- src/VectorWeapon.js
+++ src/VectorWeapon.js
@@ -1,4 +1,4 @@
-import { rectangle, group, rasterize } from './pixelShapes.js?v=18';
+import { rectangle, group, rasterize } from './pixelShapes.js?v=19';
 
 function degToRad(degrees) {
   return degrees * Math.PI / 180;
