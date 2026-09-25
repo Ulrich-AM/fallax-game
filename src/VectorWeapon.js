7a5a72c712909e272bc79b64e0d84a6899115019
Ulrich AM <ulrichspaceprogram@gmail.com>
2026-09-25 20:05:58 +0800

    Align weapon renderer dependencies for v23

--- src/VectorWeapon.js
+++ src/VectorWeapon.js
@@ -1,4 +1,4 @@
-import { rectangle, group, rasterize } from './pixelShapes.js?v=22';
+import { rectangle, group, rasterize } from './pixelShapes.js?v=23';
 
 function degToRad(degrees) {
   return degrees * Math.PI / 180;
