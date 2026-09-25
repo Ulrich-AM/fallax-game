a4a704b48d718534baa4721d48b07fcd30a72021
Ulrich AM <ulrichspaceprogram@gmail.com>
2026-09-25 10:54:01 +0800

    Version Vector dependency for Prologue attack build

--- src/VectorWeapon.js
+++ src/VectorWeapon.js
@@ -1,4 +1,4 @@
-import { rectangle, group, rasterize } from './pixelShapes.js?v=10';
+import { rectangle, group, rasterize } from './pixelShapes.js?v=11';
 
 function degToRad(degrees) {
   return degrees * Math.PI / 180;
