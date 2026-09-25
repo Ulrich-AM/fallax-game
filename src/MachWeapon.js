f9869fb8eabcf7aa043e6204efd6138ef072303b
Ulrich AM <ulrichspaceprogram@gmail.com>
2026-09-26 00:05:22 +0800

    Align dependencies for Matrix v28

--- src/MachWeapon.js
+++ src/MachWeapon.js
@@ -1,4 +1,4 @@
-import { rectangle, group, rasterize } from './pixelShapes.js?v=27';
+import { rectangle, group, rasterize } from './pixelShapes.js?v=28';
 
 function shortestAngleDelta(a, b) {
   return ((b - a + Math.PI * 3) % (Math.PI * 2)) - Math.PI;
