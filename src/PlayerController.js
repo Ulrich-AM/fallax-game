fea47252a18c22d3bba268f449a3b45a507fd10b
Ulrich AM <ulrichspaceprogram@gmail.com>
2026-09-26 00:05:12 +0800

    Align dependencies for Matrix v28

--- src/PlayerController.js
+++ src/PlayerController.js
@@ -1,4 +1,4 @@
-import { MOVEMENT as CFG } from './movementConfig.js?v=27';
+import { MOVEMENT as CFG } from './movementConfig.js?v=28';
 
 function approach(value, target, amount) {
   if (value < target) return Math.min(value + amount, target);
