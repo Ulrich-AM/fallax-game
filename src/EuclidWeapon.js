f8164d107651d8d3db8523efe6cba5c1e7419cf5
Ulrich AM <ulrichspaceprogram@gmail.com>
2026-09-25 19:51:42 +0800

    Align weapon renderer cache for v22

--- src/EuclidWeapon.js
+++ src/EuclidWeapon.js
@@ -1,4 +1,4 @@
-import { rectangle, group, rasterize } from './pixelShapes.js?v=20';
+import { rectangle, group, rasterize } from './pixelShapes.js?v=22';
 
 function rayCircleHit(originX, originY, dirX, dirY, maxDistance, circleX, circleY, radius) {
   const ox = originX - circleX;
