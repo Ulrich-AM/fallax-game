c16fdeeb5f4e8efc0d9e24545d2088361f8cfeea
Ulrich AM <ulrichspaceprogram@gmail.com>
2026-09-25 20:14:50 +0800

    Align weapon renderer dependencies for v24

--- src/EuclidWeapon.js
+++ src/EuclidWeapon.js
@@ -1,4 +1,4 @@
-import { rectangle, group, rasterize } from './pixelShapes.js?v=23';
+import { rectangle, group, rasterize } from './pixelShapes.js?v=24';
 
 function rayCircleHit(originX, originY, dirX, dirY, maxDistance, circleX, circleY, radius) {
   const ox = originX - circleX;
