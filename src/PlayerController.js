cbed2740b8a08bdc199ebc549644838eff39073e
Ulrich AM <ulrichspaceprogram@gmail.com>
2026-09-25 11:27:04 +0800

    Version player controller for Euclid build

--- src/PlayerController.js
+++ src/PlayerController.js
@@ -1,4 +1,4 @@
-import { MOVEMENT as CFG } from './movementConfig.js?v=13';
+import { MOVEMENT as CFG } from './movementConfig.js?v=14';
 
 function approach(value, target, amount) {
   if (value < target) return Math.min(value + amount, target);
