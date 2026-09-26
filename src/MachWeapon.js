0371b8f6b8a4b6c54d1f3c2e434d1b44f62e2b5f
Ulrich AM <ulrichspaceprogram@gmail.com>
2026-09-26 11:22:34 +0800

    Align audio build dependencies to v29

--- src/MachWeapon.js
+++ src/MachWeapon.js
@@ -1,4 +1,4 @@
-import { rectangle, group, rasterize } from './pixelShapes.js?v=28';
+import { rectangle, group, rasterize } from './pixelShapes.js?v=29';
 
 function shortestAngleDelta(a, b) {
   return ((b - a + Math.PI * 3) % (Math.PI * 2)) - Math.PI;
