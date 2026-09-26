8902b9a2777e82652b76a9d87ccab67b1f0c0410
Ulrich AM <ulrichspaceprogram@gmail.com>
2026-09-26 16:11:13 +0800

    Align dependencies for v33

--- src/PlayerController.js
+++ src/PlayerController.js
@@ -1,4 +1,4 @@
-import { MOVEMENT as CFG } from './movementConfig.js?v=32';
+import { MOVEMENT as CFG } from './movementConfig.js?v=33';
 
 function approach(value, target, amount) {
   if (value < target) return Math.min(value + amount, target);
