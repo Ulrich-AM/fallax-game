833be7fa8a2c2fee2428aee2850e695e6b38c1e1
Ulrich AM <ulrichspaceprogram@gmail.com>
2026-09-26 16:11:16 +0800

    Align dependencies for v33

--- src/VectorWeapon.js
+++ src/VectorWeapon.js
@@ -1,4 +1,4 @@
-import { rectangle, group, rasterize } from './pixelShapes.js?v=32';
+import { rectangle, group, rasterize } from './pixelShapes.js?v=33';
 
 function degToRad(degrees) {
   return degrees * Math.PI / 180;
